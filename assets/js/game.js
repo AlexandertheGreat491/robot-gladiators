b2683f89b603401d6bd8d7d7c92ade8ee8c82bd4
agvan <agvandyke25@gmail.com>
2022-05-06 17:03:35 -0500

    created if/else statements for confirmSkip variable

--- assets/js/game.js
+++ assets/js/game.js
@@ -18,10 +18,12 @@ else {
 //Log multiple values at once
 //Browser is being told to hold the values for these variables
 console.log(playerName, playerAttack, playerHealth);
-
+//defining starting values for enemyName, enemyHealth, & enemyAttack variables
 var enemyName = "Roborto";
 var enemyHealth = 50;
 var enemyAttack = 12;
+//defining starting value for playerMoney variable
+var playerMoney = 10;
 //Define "fight" function
 var fight = function() {
     //Alert players that they are starting the round
@@ -57,7 +59,19 @@ if (promptFight === "fight" || promptFight === "FIGHT") {
     }
     // if player choses to skip
   } else if (promptFight === "skip" || promptFight === "SKIP") {
-    window.alert(playerName + " has chosen to skip the fight!");
+    //confirm player wants to skip
+    var confirmSkip= window.confirm("Are you sure you'd like to quit?");
+
+    // if yes (true), leave fight
+    if (confirmSkip) {
+        window.alert(playerName + "has decided to skip this fight. Goodbye!");
+        //subtract money from playerMoney for skipping
+        playerMoney = playerMoney - 2;
+    }
+    //if no (false), ask question again by running fight() again
+    else {
+        fight();
+    }
   } else {
     window.alert("You need to choose a valid option. Try again!");
   }
