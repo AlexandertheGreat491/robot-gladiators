7296592ea413d5c5e4790d85fcdc50c031fb44f0
agvan <agvandyke25@gmail.com>
2022-05-06 16:05:03 -0500

    created promptFight variable w/ a window.prompt value

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,10 +1,14 @@
 var playerName = window.prompt("What is your robot's name?");
+//Define starting value of playerHealth variable
 var playerHealth = 100;
 // check to see if the value of the playerHealth variable is greater than 0
+//condition for playerHealth variable
 if (playerHealth > 0) {
     console.log("Your player is still alive!");
 }
+//Define starting value of playerAttack
 var playerAttack = 10;
+//condition for 
 if (playerHealth === 0) {
     console.log("This will not run.")
 }
@@ -45,6 +49,8 @@ if (enemyHealth <= 0) {
 else {
     window.alert(enemyName + "still has" + enemyHealth + "health left.");
 }
+//assigning a window.prompt value to the promptFight variable
+var promptFight = window.prompt("Would you like to FIGHT or SKIP this battle? Enter 'FIGHT' or 'SKIP' to choose.");
 };
 //Call "fight" function
 fight();
